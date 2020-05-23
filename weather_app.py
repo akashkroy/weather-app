8d861d5f8dec7a06afc87f1f5fd5ce3a0e8b617d
akashkroy <akashkroy0@gmail.com>
2020-05-23 16:56:10 -0400

    Takes Command Line Argument Now

--- weather_app.py
+++ weather_app.py
@@ -1,16 +1,9 @@
 #! /usr/bin/python3
 
 import requests
-import argparse
+import sys
 
-def parser():
-    parser = argparse.ArgumentParser(description="A simple Weather App")
-    return parser.parse_args()
-
-user_input = parser()
-print("GRRR")
-print(user_input) 
-
+#Takes the City Name as Command Line Argument
 
 def weather_city(city):
     api_url = "http://api.weatherapi.com/v1/current.json?key=381d17d37e344cea8ba90809202305&q="
@@ -18,8 +11,6 @@ def weather_city(city):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        #for x,y in data.items():
-            #print(x,y)
         print("*Location: {}".format(data["location"]["name"]))
         print("*Temperature (in Celcius): {}".format(data["current"]["temp_c"]))
         print("*Feels Like : {}".format(data["current"]["feelslike_c"]))
@@ -29,4 +20,8 @@ def weather_city(city):
         print()
         print("*Last Updated : {}".format(data["current"]["last_updated"]))
     
-#weather_city("kolkata")
+
+user_input = sys.argv[1:]
+user_input = " ".join(user_input)
+
+weather_city(user_input)
